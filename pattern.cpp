12ed35768a76d815554811f7c75d93b75fc3c6c7
adityakanojia <kanojiaaditya800@gmail.com>
2025-05-19 15:29:33 +0530

    3 commit

--- pattern.cpp
+++ pattern.cpp
@@ -11,7 +11,40 @@ void numberPattern (int number) { // outer loop will run till number
     }
 }
 
+void triangleSquarePattern (int number){
+    for(int i = 1; i<=number; i++){
+        for(int j=1; j<=number; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }  
+}
+
+void pyrammidTriangle (int number){
+    for(int i = 1; i <= number;i++){ // number of lines pattern will be formed
+        for(int j=1;j<=i;j++){ // will run untill i 
+            cout << "*";  // print * for example when i = 1 innerloop will print * and when i = 2 innerloop wi;; run twice and print ** 
+        }
+        cout << endl; // after 
+    }
+}
+
+void alphabetSquare (int number) {
+    for(int i = 1;i<=number;i++){
+        char ch = 'A';
+        for (int j = 1; j <= number; j++)
+        {
+            cout << ch;
+            ch = ch + 1;
+        }
+        
+        cout << endl;
+    }
+}
 int main() {
     // numberPattern(4);
+    // triangleSquarePattern(5);
+    // pyrammidTriangle(5);
+    alphabetSquare(5);
     return 0;
 }
